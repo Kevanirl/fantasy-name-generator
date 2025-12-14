6dedecfe76baa2c436bb49418bce58c64cbe01d9
Kevanirl <asmellyhamster@gmail.com>
2025-12-14 06:58:27 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-// Random first names inspired by WoW style
+// Random first names inspired by WoW/fantasy style
 const firstNames = [
     "Elunaria","Stormwarden","Gorgrim","Thalorien","Sylvalis",
-    "Zulgar","Lyraen","Valdrek","Morrath","Eldaeris","Ser","Aeliana","Balthor","Caelith","Daeryn","Elowen","Faelar","Gorath","Haleth","Isolde","Jareth","Kael","Lyra","Maelis","Nymeria","Orin","Peregrin","Quenara","Rhiannon","Sylas","Thalor","Ulric","Vaelin","Wynter","Xandor","Ysolde","Zerath"
+    "Zulgar","Lyraen","Valdrek","Morrath","Eldaeris","Ser","Aeliana",
+    "Balthor","Caelith","Daeryn","Elowen","Faelar","Gorath","Haleth",
+    "Isolde","Jareth","Kael","Lyra","Maelis","Nymeria","Orin","Peregrin",
+    "Quenara","Rhiannon","Sylas","Thalor","Ulric","Vaelin","Wynter",
+    "Xandor","Ysolde","Zerath"
 ];
 
 // Fantasy style last names
 const lastNames = [
     "Ironfist","Dawnblade","Shadowmere","Stormcaller","Bloodforge",
-"Windwhisper","Nightshade","Starwatcher","Frostweaver","Shadowhunter""Amberfall","Brightwood","Cinderbane","Duskbringer","Ebonhart","Frostveil","Glimmerforge","Hawkridge","Ironbane","Jadeeye","Korrath","Lunaris","Moonshadow","Nightbreeze","Oakenheart","Pyrelight","Quickwater","Ravencrest","Stormrider","Thornfield","Umbermoor","Valewind","Wintermere","Xyrath","Yarrowind","Zephyros"
+    "Windwhisper","Nightshade","Starwatcher","Frostweaver","Shadowhunter",
+    "Amberfall","Brightwood","Cinderbane","Duskbringer","Ebonhart","Frostveil",
+    "Glimmerforge","Hawkridge","Ironbane","Jadeeye","Korrath","Lunaris",
+    "Moonshadow","Nightbreeze","Oakenheart","Pyrelight","Quickwater","Ravencrest",
+    "Stormrider","Thornfield","Umbermoor","Valewind","Wintermere","Xyrath",
+    "Yarrowind","Zephyros"
 ];
 
+// Generate a completely random full name
 function generateFullName() {
     const randomFirst = firstNames[Math.floor(Math.random() * firstNames.length)];
     const randomLast  = lastNames[Math.floor(Math.random() * lastNames.length)];
@@ -17,6 +27,7 @@ function generateFullName() {
     document.getElementById("fullNameOutput").textContent = fullName;
 }
 
+// Generate a last name to match a user-provided first name
 function generateMatchingLastName() {
     const firstName = document.getElementById("firstNameInput").value.trim();
     if (firstName === "") {
@@ -25,7 +36,6 @@ function generateMatchingLastName() {
     }
 
     const randomLast = lastNames[Math.floor(Math.random() * lastNames.length)];
-
     const matchedName = firstName + " " + randomLast;
     document.getElementById("matchedLastNameOutput").textContent = matchedName;
 }
