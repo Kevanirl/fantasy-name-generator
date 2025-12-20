23f759adc10b7c27ae9b3a6b632399f26b93ab62
Kevanirl <asmellyhamster@gmail.com>
2025-12-20 19:41:26 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,12 @@
+// --------------------
+// Fantasy Name Generator
+// --------------------
+
 // Random first names
-const firstNames = [
-    "Elunaria","Stormwarden","Gorgrim","Thalorien","Sylvalis",
-    "Zulgar","Lyraen","Valdrek","Morrath","Eldaeris","Ser","Aeliana",
-    "Balthor","Caelith","Daeryn","Elowen","Faelar","Gorath","Haleth",
-    "Isolde","Jareth","Kael","Lyra","Maelis","Nymeria","Orin","Peregrin",
-    "Quenara","Rhiannon","Sylas","Thalor","Ulric","Vaelin","Wynter",
-    "Xandor","Ysolde","Zerath","Aine", "Bran", "Cian", "Dara", "Eira", "Fionn", "Grainne",
-    "Kael", "Lir", "Maeve", "Niamh", "Orla", "Ronan",
-    "Siobhan", "Taran", "Vevina", "Wynne", "Yseult", "Zaira","Aranel", "Thalion", "Eldarion", "Faramir",
-    "Elrohir", "Arwen", "Celeborn", "Finrod", "Gildor", "Haldir",
-    "Isildur", "Luthien", "Maedhros", "Nimrodel", "Oropher", "Peregrin",
-    "Quendolin", "Rohan", "Silmarien", "Tauriel", "Ulmo", "Voronwe", "Yavanna","Darrow", "Eo", "Mustang", "Sevro", "Cassius", "Roque",
-    "Pax", "Lysander", "Victra", "Rhonna", "Ephraim",
-    "Lyria", "Fitchner", "Antonia", "Titus", "Quinn", "Apollo",
-    "Atlas", "Electra", "Narol", "Dancer", "Harmony", "Mickey",
-    "Octavia", "Adrius", "Tiberius", "Aja", "Ragnar","Fennec",
-    "Jyn", "Cassian","Aeron", "Branwen", "Cai", "Dafydd", "Eira", "Ffion", "Gwydion",
-    "Heulwen", "Idris", "Lleu", "Mabon", "Nerys", "Owain", "Rhiannon",
-    "Seren", "Taliesin", "Tegan", "Urien", "Ysbail", "Elen", "Maelon", "Cú Chulainn", "Fionn", "Lugh", "Brigid",
-    "Deirdre", "Medb", "Conn", "Nuada", "Morrigan", "Bres",
-    "Ériu", "Dagda", "Étaín", "Macha", "Cernunnos", "Ailill",
-    "Éogan", "Cliodhna", "Jon", "Tyrion", "Arya", "Sansa", "Bran", "Cersei",
-    "The Idiot", "Robb", "Jorah", "Theon",
-    "Gendry", "Brienne", "Sandor", "Davos", "Eddard", "Petyr",
-    "Melisandre", "Ygritte", "Oberyn", "Rhaegar", "Asha", "Aelar", "Eldrin", "Faelar", "Therion", "Sylvaris", "Lorien", "Arannis", "Erevan",
-    "Mythrandir","Caelthas","Ithilion","Vaelis","Rynar","Elion","Thamior","Aeris","Lioren","Sylrin","Vaelin","Ithryn","Caeris",
-    "Elthir","Nerith","Thyren","Saeris","Mythil","Faelis","Rilith","Eryndor","Quendolin", "Brevik",
-    "Calithra", "Sigurd", "Hakon", "Fenrir",
-    "Corvath",
-    "Dravena",
-    "Drovak",
-    "Elvynna",
-    "Harkan",
-    "Jorvath",
-    "Kaelith",
-    "Kaelric",
-    "Lysera",
-    "Maldrin",
-    "Morwen",
-    "Nysera",
-    "Soryn",
-    "Sorielle",
-    "Thalira",
-    "Tharion",
-    "Valmir",
-    "Veyra"
-];
+const firstNames = [/* your firstNames array as before */];
 
 // Random last names
-const lastNames = [
-    "Ironfist","Dawnblade","Shadowmere","Stormcaller","Bloodforge",
-    "Windwhisper","Nightshade","Starwatcher","Frostweaver","Shadowhunter",
-    "Amberfall","Brightwood","Cinderbane","Duskbringer","Ebonhart","Frostveil",
-    "Glimmerforge","Hawkridge","Ironbane","Jadeeye","Korrath","Lunaris",
-    "Moonshadow","Nightbreeze","Oakenheart","Pyrelight","Quickwater","Ravencrest",
-    "Stormrider","Thornfield","Umbermoor","Valewind","Wintermere","Xyrath",
-    "Yarrowind","Zephyros","Evenstar","Lightbringer","Stormwind","Oakenshield","Silverleaf",
-    "Ironfoot","Shadowfax","Windrunner","Moonwhisper","Starfire",
-    "Dawntracker","Nightbreeze","Frosthelm","Goldenbranch","Stonehelm",
-    "Brightblade","Swiftarrow","Sunshadow","Mistwalker","Darkwater",
-    "au Andromedus","au Augustus","au Barca","au Bellona",
-    "au Lune","au Telemanus","au Trachus","au Faran",
-    "au Valii-Rath","au Raa","of Lagalos","of Lykos",
-    "ti Horn","ti Nakamura","ap Rhys","ferch Gwilym","Dafyddson","Llywelyn","ab Owain",
-    "ap Gruffydd","Penrose","Gwynedd","Trevelyan","ap Hywel",
-    "ap Cadwaladr","of Dyfed","of Powys","ab Morgan","ap Tudor",
-    "Penbryn","of Caerleon","ap Meurig","ap Llewelyn","af Angharad","Tír na nÓg","Arryn","Tully","Bolton","Mormont","Clegane",
-    "Hightower","Seaworth","Reed","Baelish","Blackfyre","Dayne",
-    "Florent","Karstark","Harrenhal","Velaryon","Durrandon",
-    "Redwyne","Moonwhisper","Silverleaf","Dawnrunner","Starfall","Nightbreeze",
-    "Sunweaver","Shadowgrove","Frostpetal","Lightbranch","Windstride", "Bloodscar", "Aradoth","Blackthorn",
-    "Bloodfallow", "Thornwood",
-    "Duskwarden",
-    "Frostmarrow",
-    "Grimhold",
-    "Ironveil",
-    "Nightbane",
-    "Ravenscar",
-    "Shadowrend",
-    "Stormvein"
-];
+const lastNames = [/* your lastNames array as before */];
 
 // Functions
 function generateFullName() {
@@ -88,9 +15,8 @@ function generateFullName() {
     const output = document.getElementById("fullNameOutput");
     output.textContent = randomFirst + " " + randomLast;
 
-    // Trigger fade-in animation
     output.style.animation = 'none';
-    void output.offsetWidth; // restart animation
+    void output.offsetWidth;
     output.style.animation = 'fadeInUp 0.5s forwards';
 }
 
@@ -104,9 +30,8 @@ function generateMatchingLastName() {
     const output = document.getElementById("matchedLastNameOutput");
     output.textContent = firstName + " " + randomLast;
 
-    // Trigger fade-in animation
     output.style.animation = 'none';
-    void output.offsetWidth; // restart animation
+    void output.offsetWidth;
     output.style.animation = 'fadeInUp 0.5s forwards';
 }
 
@@ -114,4 +39,66 @@ function generateMatchingLastName() {
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("fullNameBtn").addEventListener("click", generateFullName);
     document.getElementById("matchLastBtn").addEventListener("click", generateMatchingLastName);
+    initializeMap(); // Initialize the map
 });
+
+// --------------------
+// Interactive Map
+// --------------------
+let map;
+let markers = [];
+
+// Example bad words list (add more as needed)
+const badWords = ["badword1", "badword2", "idiot", "curseword"];
+
+// Check for bad words in input
+function containsBadWords(text) {
+    const lowerText = text.toLowerCase();
+    return badWords.some(word => lowerText.includes(word));
+}
+
+function initializeMap() {
+    map = L.map('map').setView([51.505, -0.09], 3); // Centered globally
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Add marker on button click
+    document.getElementById("addMarkerBtn").addEventListener("click", () => {
+        const nameInput = document.getElementById("locationNameInput").value.trim();
+        if (!nameInput) {
+            alert("Please enter a location name.");
+            return;
+        }
+        if (containsBadWords(nameInput)) {
+            alert("Inappropriate words are not allowed.");
+            return;
+        }
+
+        const center = map.getCenter(); // place marker at center of map
+        const marker = L.marker(center).addTo(map).bindPopup(nameInput);
+        markers.push(marker);
+
+        // Clear input
+        document.getElementById("locationNameInput").value = "";
+    });
+
+    // Optional: add marker on map click
+    map.on('click', function(e) {
+        const nameInput = document.getElementById("locationNameInput").value.trim();
+        if (!nameInput) {
+            alert("Please enter a location name before clicking the map.");
+            return;
+        }
+        if (containsBadWords(nameInput)) {
+            alert("Inappropriate words are not allowed.");
+            return;
+        }
+
+        const marker = L.marker(e.latlng).addTo(map).bindPopup(nameInput);
+        markers.push(marker);
+        document.getElementById("locationNameInput").value = "";
+    });
+}
